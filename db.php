940ca59b8bd0416e5701a4eafe825dce588f7d2b
Ramesh Kumar <trkrameshkumar@gmail.com>
2013-08-19 15:30:32 +0530

    Event admin page is successfully completed

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-mysql_select_db("drestine", $con);
+mysql_select_db("drestein", $con);
 
 
 
--- drestein.sql
+++ drestein.sql
@@ -17,10 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `drestein`
+-- Database: `drestein_reg`
 --
-CREATE DATABASE IF NOT EXISTS `drestein` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `drestein`;
+CREATE DATABASE IF NOT EXISTS `drestein_reg` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `drestein_reg`;
 
 -- --------------------------------------------------------
 
